f53f7d8f388aa0f41b6a0c9c61816df15ae56d96
Shreyansh Priyadarshi <73597266+iamspriyadarshi@users.noreply.github.com>
2021-01-17 13:46:09 +0530

    Add files via upload

---
+++ Inferring_mRNA_from_Protein.py
@@ -0,0 +1,17 @@
+# Written by Shreyansh Priyadarshi
+# This code can infer mRNA from protein.
+
+# Enter protein sequence below.
+protein = ''
+count_RNA = 1
+
+amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
+for i in protein:
+    s = 0
+    for j in amino_acids:
+        if i == j:
+            s += 1
+    if s != 0:
+        count_RNA *= s
+
+print(count_RNA*3 % 1000000)

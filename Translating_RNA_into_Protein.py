e3519cbd3769e0e17266f52058fd950553552508
Shreyansh Priyadarshi <73597266+iamspriyadarshi@users.noreply.github.com>
2021-01-17 13:19:25 +0530

    Add files via upload

---
+++ Translating_RNA_into_Protein.py
@@ -0,0 +1,23 @@
+# Written by Shreyansh Priyadarshi.
+# This code can convert RNA into protein.
+
+# Enter RNA sequence below.
+RNA = ''
+
+def rna_to_protein(rna: str):
+    bases = ['U', 'C', 'A', 'G']
+    codons = [a+b+c for a in bases for b in bases for c in bases]
+    amino_acids = 'FFLLSSSSYY**CC*WLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG'
+    codon_table = dict(zip(codons, amino_acids))
+    SOLUTION = ''
+
+    for i in range(0, len(rna), 3):
+        if codon_table[rna[i] + rna[i + 1] + rna[i + 2]] != '*':
+            SOLUTION += codon_table[rna[i] + rna[i + 1] + rna[i + 2]]
+        else:
+            break
+
+    return SOLUTION
+
+
+print(rna_to_protein(RNA))
